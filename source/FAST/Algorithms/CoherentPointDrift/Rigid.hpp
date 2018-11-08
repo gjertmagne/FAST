261574a70d818032722809a916dade5606a5d82f
Gjert Magne Knutsen <gjert_magne@hotmail.com>
2018-11-08 17:48:16 +0100

    Added separate class for rigid registration and restructured CPD class to fit the new class

---
+++ source/FAST/Algorithms/CoherentPointDrift/Rigid.hpp
@@ -0,0 +1,38 @@
+#ifndef FAST_AFFINE_H
+#define FAST_AFFINE_H
+
+
+#include "FAST/AffineTransformation.hpp"
+#include "FAST/ProcessObject.hpp"
+#include "FAST/Data/Mesh.hpp"
+#include "FAST/Algorithms/CoherentPointDrift/CoherentPointDrift.hpp"
+
+namespace fast {
+
+    class FAST_EXPORT CoherentPointDriftRigid: public CoherentPointDrift {
+    FAST_OBJECT(CoherentPointDriftRigid);
+    public:
+        CoherentPointDriftRigid();
+        void expectation(MatrixXf& fixedPoints, MatrixXf& movingPoints) override;
+        void maximization(MatrixXf& fixedPoints, MatrixXf& movingPoints) override;
+        void initializeVarianceAndMore() override;
+
+    private:
+        MatrixXf mProbabilityMatrix;            // P
+        MatrixXf mPt1;                          // Colwise sum of P, then transpose
+        MatrixXf mP1;                           // Rowwise sum of P
+        MatrixXf mRotation;                     // R
+        MatrixXf mTranslation;                  // t
+        double mObjectiveFunction;              // Q
+        double mVariance;                       // sigma^2
+        double mIterationError;                 // Change in error from iteration to iteration
+        float mNp;                              // Sum of all elements in P
+        TransformationType mTransformationType;
+        //        double mScale;                          // s
+
+    };
+
+}
+
+
+#endif //FAST_AFFINE_H
